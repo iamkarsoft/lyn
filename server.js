9c5a954aeb6f394a931c5ebe956b92d6c0fcc4fa
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-10 23:58:43 +0000

    core: redirect

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req,res)=>{
- 
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
   //set header content type
-  res.setHeader('Content-Type','text/html');
+  res.setHeader("Content-Type", "text/html");
   //send an html file
-  fs.readFile('./views/index.html',(err,data)=>{
-    if(err){
+  //path to get url
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader('Location','/about');
+      res.end();
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
       console.log(err);
       res.end();
-
-    }else{
+    } else {
       // res.write(data);
       res.end(data);
     }
-  })
-
-
+  });
 });
 
-server.listen(4000, 'localhost',()=>{
-  console.log('listening on port 4000')
-})
+server.listen(4000, "localhost", () => {
+  console.log("listening on port 4000");
+});
