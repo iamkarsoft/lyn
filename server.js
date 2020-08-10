01305203f13284b468284da210dcb13a1bf177c1
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-10 23:30:36 +0000

    core: sending html to browser

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req,res)=>{
  
   //set header content type
-  res.setHeader('Content-Type','text/plain');
-  
-  res.write('hello, ninjas');
-  res.end();
+  res.setHeader('Content-Type','text/html');
+  //send an html file
+  fs.readFile('./views/index.html',(err,data)=>{
+    if(err){
+      console.log(err);
+      res.end();
+
+    }else{
+      // res.write(data);
+      res.end(data);
+    }
+  })
+
+
 });
 
 server.listen(4000, 'localhost',()=>{
