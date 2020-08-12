4a0b852b8a8f3d7843bd95435ce52641d251c66d
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-12 00:42:54 +0000

    core: added ejs for templating engine and created form for blogpost

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 // express app
 const app = express();
 
-
+// register view engine
+app.set('view engine','ejs')
+// app.set('views', 'views')
 // listen for equests
 
 app.listen(4500);
@@ -11,21 +13,21 @@ app.listen(4500);
 
 //routes
 app.get('/',(req,res)=>{
-res.sendFile("./views/index.html", { root: __dirname });
+res.render('index')
 })
 
 
 app.get('/about',(req,res)=>{
-  res.sendFile('./views/about.html', {root: __dirname });
+  res.render('about')
 })
 
 // redirects
-app.get('/about-us',(req,res)=>{
-  res.redirect('/about');
+app.get('/blogs/create',(req,res)=>{
+  res.render('create');
 })
 
 // 404 page
 app.use((req,res)=>{
-  res.status(404).sendFile('./views/404.html', {root: __dirname });
+  res.status(404).render('404');
 
 })
