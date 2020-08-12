9ee33d790fda6d41528b842c6e318a900906a793
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-12 00:16:31 +0000

    package: added express

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require('lodash');
+
 const server = http.createServer((req, res) => {
+
   //set header content type
   res.setHeader("Content-Type", "text/html");
   //send an html file
