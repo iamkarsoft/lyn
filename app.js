32bf3fabcd35f9e01abdef9651ae7e709b90b11e
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-14 06:20:21 +0000

    feat: connect to db , create model and schema, then add data and retrieve date to db

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-const DBURI = mongodb+srv://kofiramos:<password>@lyn.4lpm6.mongodb.net/<dbname>?retryWrites=true&w=majority;
+const dbURI = 'mongodb+srv://kofiramos:hellofresh@lyn.4lpm6.mongodb.net/lyn?retryWrites=true&w=majority';
 
-
+//connect to db
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result)=>app.listen(4500)).catch((err)=>console.log(err));
 // express app
 const app = express();
 
@@ -12,8 +15,6 @@ app.set('view engine','ejs')
 // app.set('views', 'views')
 // listen for equests
 
-app.listen(4500);
-
 // middleware and assets
 app.use(express.static('public'))
 
@@ -21,6 +22,35 @@ app.use(express.static('public'))
 app.use(morgan('dev'));
 
 //routes
+
+//connect to mongoose
+app.get('/add-blog',(req,res)=>{
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my blog',
+    body: 'new more'
+  });
+
+  blog.save()
+  .then((result)=>{
+    res.send(result)
+  })
+  .catch((err)=>console.log(err));
+})
+// get all blogs
+app.get('/all-blogs',(req,res)=>{
+  Blog.find()
+  .then((result)=>{
+    res.send(result)
+  }).catch((err)=>console.log(err));
+})
+
+// find a single blog
+app.get('/single-blog',(req,res)=>{
+  Blog.findById('5f362b61bf7dcb2414c33a40')
+  .then((result)=>{res.send(result)})
+  .catch((err)=>console.log(err));
+})
 app.get('/',(req,res)=>{
   const blogs = [
     {title: 'Yoshi', snippet:'lorem10dfsfsdf fas'},
