736a5d0403d0d40063d128b8f4c2107c99237a04
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-15 18:09:52 +0000

    refactor: mvc'ed the app

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,56 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const Blog = require('./../models/blog');
+
+
+// controllers
+const blogController = require('../controllers/blogController');
 
 // get all blog post
-router.get('/',(req,res)=>{
-  Blog.find().sort({createdAt: -1})
-  .then((result)=>{
-    res.render('index',{title: 'All Blogs', blogs: result})
-  })
-  .catch((err)=>console.log(err))
-})
+router.get('/',blogController.blog_index)
 
 // show create form
-router.get('/create',(req,res)=>{
-  res.render('create', {title: "blog"});
-})
+router.get('/create',blogController.blog_create);
 
 //create new post
-router.post('/',(req,res)=>{
-  const blog = new Blog(req.body);
-  blog.save()
-  .then((result)=>{
-    res.redirect('/blogs');
-  }).catch((err)=>{
-    console.log(err)
-  })
-})
+router.post('/', blogController.blog_post)
 
 // show single blog
-router.get('/:id',(req,res)=>{
-  const id = req.params.id;
-  Blog.findById(id)
-  .then((result)=>{
-    res.render('details',{blog: result, title: 'Blog Details'})
-  })
-  .catch(err=>{
-    console.log(err)
-  })
-})
+router.get('/:id',blogController.blog_details)
+
 
 // delete blog
-router.delete('/:id',(req,res)=>{
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-  .then((result)=>{
-    res.json({
-      redirect: '/blogs'
-    });
-  })
-  .catch(err=>
-    console.log(err)
-  )
-})
+router.delete('/:id',blogController.blog_delete)
 
 module.exports = router;
