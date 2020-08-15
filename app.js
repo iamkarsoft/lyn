427fe546dc32da8f000e964e27ac75c13de162a2
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-08-15 17:50:32 +0000

    refactor: using express router

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 const dbURI = 'mongodb+srv://kofiramos:hellofresh@lyn.4lpm6.mongodb.net/lyn?retryWrites=true&w=majority';
+
+
 
 //connect to db
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result)=>app.listen(4500)).catch((err)=>console.log(err));
@@ -25,6 +27,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 //routes
+app.use(blogRoutes)
 
 //connect to mongoose
 
@@ -40,52 +43,7 @@ app.get('/about',(req,res)=>{
 // redirects
 //blogs
 
-app.get('/blogs',(req,res)=>{
-  Blog.find().sort({createdAt: -1})
-  .then((result)=>{
-    res.render('index',{title: 'All Blogs', blogs: result})
-  })
-  .catch((err)=>console.log(err))
-})
-app.get('/blogs/create',(req,res)=>{
-  res.render('create', {title: "blog"});
-})
 
-//post request
-app.post('/blogs',(req,res)=>{
-  const blog = new Blog(req.body);
-  blog.save()
-  .then((result)=>{
-    res.redirect('/blogs');
-  }).catch((err)=>{
-    console.log(err)
-  })
-})
-
-
-app.get('/blogs/:id',(req,res)=>{
-  const id = req.params.id;
-  Blog.findById(id)
-  .then((result)=>{
-    res.render('details',{blog: result, title: 'Blog Details'})
-  })
-  .catch(err=>{
-    console.log(err)
-  })
-})
-
-app.delete('/blogs/:id',(req,res)=>{
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-  .then((result)=>{
-    res.json({
-      redirect: '/blogs'
-    });
-  })
-  .catch(err=>
-    console.log(err)
-  )
-})
 // 404 page similar to middleware
 app.use((req,res)=>{
   res.status(404).render('404');
